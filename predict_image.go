98196b61157b2e7284f2009f9f3ac36d72a3bd65
Sagar Mangulkar <smangul1@binghamton.edu>
2018-06-16 03:55:26 -0400

    Implement data structure to store output. Add sorting, displayMap(temporary) functions. Testing for less images.

--- predict_image.go
+++ predict_image.go
@@ -1,50 +1,72 @@
 package main
 
 import (
-	"fmt"
-
 	"bufio"
+	"fmt"
+	cl "github.com/mpmlj/clarifai-client-go"
 	"log"
 	"os"
-	//"io/ioutil"
-
-	cl "github.com/mpmlj/clarifai-client-go"
+	"sort"
 )
 
 const (
 	apiKey = "d6d623dad6894b54b6a5feb980edc339"
 )
 
+type imageValue struct {
+	image string
+	value float64
+}
+
 func main() {
 
+	mp := make(map[string][]imageValue)
 	file, err := os.Open("images.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		image := scanner.Text()
 		fmt.Println(image)
 		resp := PredictImage(image)
-		cl.PP(resp)
-		fmt.Println()
+
+		output := resp.Outputs[0].Data.Concepts
+		for _, element := range output {
+			iv := imageValue{image: image, value: element.Value}
+			mp[element.Name] = insertSort(mp[element.Name], iv)
+		}
 	}
+
+	displayMap(mp)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	//image := "https://c3.staticflickr.com/4/3372/3557249560_fa83b3c878_o.jpg"
+}
+
+func displayMap(mp map[string][]imageValue) {
+	for key, value := range mp {
+		fmt.Println(key)
+		fmt.Println(value)
+		fmt.Println()
+	}
+}
+
+func insertSort(data []imageValue, el imageValue) []imageValue {
+	index := sort.Search(len(data), func(i int) bool { return data[i].value < el.value })
+	data = append(data, imageValue{})
+	copy(data[index+1:], data[index:])
+	data[index] = el
+	return data
 }
 
 func PredictImage(image string) *cl.Response {
 	var err error
 	var sess *cl.Session
 	sess = cl.NewApp(apiKey)
-	fmt.Println(sess)
 	data := cl.InitInputs()
-	//	_ = data.AddInput(cl.NewImageFromURL("https://farm3.staticflickr.com/568/21452126474_ab12789b36_o.jpg"), "")
 	_ = data.AddInput(cl.NewImageFromURL(image), "")
 	data.SetModel(cl.PublicModelTravel)
 	resp, err := sess.Predict(data).Do()
